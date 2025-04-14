daa27aed7fd64f0478c701c212f214618223eb49
Jillypsh <jmdlirazan22@src.pshs.edu.ph>
2025-04-14 20:06:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+document.getElementById("usernameForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    setCookie("username", username, 1);
+    location.reload();
+});
+
+let storedUsername = getCookie("username");
+if (storedUsername) {
+    document.getElementById("greeting").innerText = "Welcome back, " + storedUsername + "!";
+}
